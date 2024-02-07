c093d119839f42e99cd029b1a2e33c24b3ca7b7a
Fernando Quintanilla <df.quintanillah@gmail.com>
2024-02-06 20:41:44 -0600

    feat: :sparkles: Add React integration and update dependencies
    Nueva seccion

---
+++ src/components/react/DownloadCV.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ButtonDownloadCV() {
+  return (
+    <>
+      <div className="flex-row bg-botoncito text-white cursor-pointer bg-hover:bg-botoncito_2 focus:ring-4 focus:outline-none font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center mr-2 mb-2 hover:shadow-lg transition-all duration-200 ease-in-out hover:scale-110  gap-x-2 opacity-90 hover:opacity-100">
+        <button>Descargar CV</button>
+      </div>
+    </>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        botoncito: "#5E60A9",
+        botoncito_2: "#FF7F50",
+      },
+    },
+  },
+  plugins: [],
+};
