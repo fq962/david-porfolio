a58a70b529947ac18a5bc47861d47d7492d22316
Fernando Quintanilla <df.quintanillah@gmail.com>
2024-09-18 22:58:37 -0600

    feat: add ExternalLinkIcon component and integrate it into CardExperience for external links

--- src/components/react/CardExperience.tsx
+++ src/components/react/CardExperience.tsx
@@ -3,15 +3,17 @@ import React from "react";
 import { useState, type MouseEvent, useCallback } from "react";
 import type { CardInterface } from "../../interfaces/CardExperience.interface";
 import Tag from "./Tag";
+import ExternalLinkIcon from "../icons/ExternalLink";
 
 export const CardExperienceComponnent: React.FC<CardInterface> = ({
   title,
   description,
   yearExperience,
-  imageURL,
+  pageURL,
   skills,
 }) => {
   const [rotation, setRotation] = useState({ x: 0, y: 0 });
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const card = e.currentTarget;
@@ -27,6 +29,11 @@ export const CardExperienceComponnent: React.FC<CardInterface> = ({
 
   const handleMouseLeave = () => {
     setRotation({ x: 0, y: 0 });
+    setIsHovered(false); // Cuando el mouse sale del componente
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true); // Cuando el mouse entra en el componente
   };
 
   return (
@@ -36,6 +43,7 @@ export const CardExperienceComponnent: React.FC<CardInterface> = ({
         className="card relative grid grid-cols-12 grid-rows-3 gap-2 rounded-lg p-4 px-5 transition-[all_400ms_cubic-bezier(0.03,0.98,0.52,0.99)_0s] will-change-transform hover:bg-cardHover hover:bg-opacity-40 hover:shadow-lg hover:backdrop-blur-md hover:backdrop-filter md:w-[750px]"
         onMouseMove={handleMouseMove}
         onMouseLeave={handleMouseLeave}
+        onMouseEnter={handleMouseEnter}
         // style={{
         //   transform: `perspective(50000px) rotateX(${rotation.x}deg) rotateY(${rotation.y}deg) scale3d(1, 1, 1)`,
         //   transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
@@ -49,28 +57,24 @@ export const CardExperienceComponnent: React.FC<CardInterface> = ({
         {/* Imagen o año de experiencia */}
         <div className="order-1 col-start-1 col-end-13 row-start-1">
           <span className="font-medium text-gray-500">{yearExperience}</span>
-
-          {/* {imageURL ? (
-            <img
-              // className="rounded-md lg:w-48 xl:w-32"
-              className="w-40 rounded-md"
-              src={imageURL}
-              alt="Site Image"
-            />
-          ) : (
-            <span className="font-medium text-gray-500">{yearExperience}</span>
-          )} */}
-          {/* <span className="text-gray-500 font-medium">{yearExperience}</span> */}
-          {/* <img className="rounded-xl" src="../../../public/fermodoroPage.png" alt="Site Image" /> */}
         </div>
-        {/* <div className="col-start-3 col-end-13 flex flex-col pl-3 xl:pl-3"> */}
-        {/* Descripcion del card */}
-        {/* <div className="col-start-3 col-end-13 row-start-2 flex flex-col pl-3"> */}
         <div className="col-start-1 col-end-13 row-start-2 flex flex-col">
-          <div className=" ">
+          <div className="flex gap-4">
             <h2 className="mb-2 text-xl font-bold text-white group-hover:text-linkGreen">
               {title}
             </h2>
+            {pageURL && (
+              <a
+                href={pageURL}
+                target="_blank"
+                rel="noreferrer"
+                className={`transform transition-opacity duration-300 ease-in-out ${
+                  isHovered ? "scale-110 opacity-100" : "scale-0 opacity-0"
+                }`}
+              >
+                <ExternalLinkIcon />
+              </a>
+            )}
           </div>
           <div className="pb-5">
             <p className="text-gray-400">{description}</p>
